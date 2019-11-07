ca4018c7346ad33ead334d71b94789dc15bb843f
Burton <burton00tw@gmail.com>
2019-11-07 21:00:53 +0800

    Create Files

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const port = 3000;
+const app = express();
+const bodyParser = require("body-parser");
+const generate = require("./rubbish_generator.js/index.js");
+
+app.use(bodyParser.urlencoded((extend = true)));
+
+//template engine
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//index page
+app.get("/", (req, res) => {
+  res.render("index");
+});
+//catch  post
+app.post("/", (req, res) => {
+  const career = req.body;
+  const words = generate(career);
+  const careerResult = {
+    engineer: false,
+    designer: false,
+    entrepreneur: false
+  };
+  if (career.option == "engineer") careerResult.engineer = true;
+  else if (career.option == "designer") careerResult.designer = true;
+  else if (career.option == "entrepreneur") careerResult.entrepreneur = true;
+  res.render("index", {
+    words: words,
+    careerResult: careerResult
+  });
+});
+
+//listen on port
+app.listen(port, () => {
+  console.log(`This express is listening on http://localhost:${port}`);
+});
---
+++ rubbish_generator.js
@@ -0,0 +1,37 @@
+function generate(career) {
+  const task = {
+    engineer: ["加個按鈕", "加新功能", "切個版", "改一點 code"],
+    designer: ["畫一張圖", "改個 logo", "順便幫忙設計一下", "隨便換個設計"],
+    entrepreneur: ["週末加班", "要能賺錢", "想個 business model", "找 VC 募錢"]
+  };
+  const phrase = [
+    "應該很簡單吧!",
+    "應該很容易吧!",
+    "應該很快吧!",
+    "應該很正常吧!"
+  ];
+  function randomMaker(num) {
+    let index = Math.floor(Math.random() * num);
+    return index;
+  }
+  let words = "身為一個";
+  if (career.option == "engineer") {
+    words += "工程師，";
+    words += task.engineer[randomMaker(task.engineer.length)];
+    words += phrase[randomMaker(phrase.length)];
+  } else if (career.option == "designer") {
+    words += "設計師，";
+    words += task.designer[randomMaker(task.designer.length)];
+    words += phrase[randomMaker(phrase.length)];
+  } else if (career.option == "entrepreneur") {
+    words += "創業家，";
+    words += task.entrepreneur[randomMaker(task.entrepreneur.length)];
+    words += phrase[randomMaker(phrase.length)];
+  } else {
+    words = "!!!請選擇職業!!!";
+  }
+  return words;
+}
+//export function
+
+module.exports = generate;
